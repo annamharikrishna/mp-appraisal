75f11759b1918377cc214567b3c32f91254a21be
Dasavanth-Alapati <111566135+Dasavanth-Alapati@users.noreply.github.com>
2023-05-11 14:54:01 +0530

    views for registration and login

--- mp_appraisal_core/appraisalentry/appraisal_manager.py
+++ mp_appraisal_core/appraisalentry/appraisal_manager.py
@@ -1,6 +1,8 @@
 # Generate the manager class for Employee Appraisal
 
 from .models import Employee, Role, EmployeeAppraisalForm
+from .serializers import EmployeeSerializer
+
 
 class EmployeeAppraisalManager:
 
@@ -20,4 +22,34 @@ class EmployeeAppraisalManager:
     # Create an helper function to return a downloadable excel response that contains employee filled appraisal data
     # input of employee id and appraisal form id
     # output of excel response
+
+    # create a registration function for employee using EmployeeSerializer
+
+    @staticmethod
+    def employee_registration(data):
+        employee = EmployeeSerializer(data=data)
+        if employee.is_valid():
+            employee.save()
+            return employee.data
+        else:
+            return employee.errors
+
+    #create a function to get employee data
+
+    @staticmethod
+    def get_employee(data):
+        employee = Employee.objects.filter(id=data.get('employee_id')).values()
+        return employee
+
+    #create a function to login employee
+
+    @staticmethod
+    def employee_login(data):
+        employee = Employee.objects.filter(employee_id=data.get('employee_id'), password=data.get('password')).values()
+        if employee:
+            return employee
+        else:
+            return False
+
+
 
--- mp_appraisal_core/appraisalentry/views.py
+++ mp_appraisal_core/appraisalentry/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from appraisalentry.appraisal_manager import EmployeeAppraisalManager
+from .appraisal_manager import EmployeeAppraisalManager
 
 
 # Create your views here.
@@ -26,3 +26,41 @@ class EmployeeAppraisalView(APIView):
             except Exception as error:
                 return Response(str(error), 500)
             return Response(response, 200)
+
+
+# generate class level views for employee registration
+
+class EmployeeRegistrationView(APIView):
+
+    def post(self, request):
+        data = request.data
+        try:
+            response = EmployeeAppraisalManager().employee_registration(data)
+        except Exception as error:
+            return Response(str(error), 500)
+        return Response({"message": 'success'}, 200)
+
+    def get(self, request):
+        data = request.data
+        try:
+            response = EmployeeAppraisalManager().get_employee(data)
+        except Exception as error:
+            return Response(str(error), 500)
+        return Response(response, 200)
+
+# generate class level views for employee login
+
+
+class EmployeeLoginView(APIView):
+
+    def post(self, request):
+        data = request.data
+        try:
+            response = EmployeeAppraisalManager().employee_login(data)
+        except Exception as error:
+            return Response(str(error), 500)
+        if response:
+            return Response(response, 200)
+        else:
+            return Response({"message": 'Invalid Credentials'}, 401)
+
