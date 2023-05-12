86c05870dd458d802cd55df87d18aa77ac5b1581
sainath9636 <harikrishna.annam@live.com>
2023-05-12 15:09:11 +0530

    Update service.js

--- mp_appraisal_web/src/components/service.js
+++ mp_appraisal_web/src/components/service.js
@@ -1,1 +1,9 @@
 // service file
+import { HttpAxiosService } from "../services/HttpService";
+import { APPRAISAL_BASE_URL } from "../config/config.environment";
+import { Urls } from "./Urls";
+const searchService = new HttpAxiosService(APPRAISAL_BASE_URL);
+
+export const searchData = ()=>{
+    return searchService.get(Urls.APPRAISAL_FORM_URL)
+}
