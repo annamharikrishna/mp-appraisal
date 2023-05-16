3791d67559d2fd908969c5c2c7b5b3af381302b1
venkatesh471 <61882686+venkatesh471@users.noreply.github.com>
2023-05-16 18:39:23 +0530

    Updated Changes

--- mp_appraisal_web_chatgpt/src/components/Dashboard.js
+++ mp_appraisal_web_chatgpt/src/components/Dashboard.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import Popup from './Popup';
-import EditForm from './EditForm';
+import React, { useState, useEffect } from "react";
+import Popup from "./Popup";
+import EditForm from "./EditForm";
 
 const Dashboard = () => {
   const [appraisals, setAppraisals] = useState([]);
@@ -9,18 +9,36 @@ const Dashboard = () => {
 
   useEffect(() => {
     // Fetch appraisals from the API and update the state
-    // fetchAppraisals();
+    fetchAppraisals();
   }, []);
 
   const fetchAppraisals = () => {
     // Make an API call to retrieve the appraisals list
     // Replace 'https://api.example.com/appraisals' with your actual API endpoint
-    fetch('https://api.example.com/appraisals')
+
+    const apiUrl =
+      "http://127.0.0.1:8000/api/appraisalentry/get_employee_appraisal_form";
+    const params = {
+      user_id: localStorage.getItem("userId"),
+    };
+
+    // Convert the params object into a query string
+    const queryString = Object.keys(params)
+      .map(
+        (key) => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`
+      )
+      .join("&");
+
+    // Append the query string to the URL
+    const url = `${apiUrl}?${queryString}`;
+    fetch(url)
       .then((response) => response.json())
       .then((data) => {
+        // Handle the response data
         setAppraisals(data);
       })
       .catch((error) => {
+        // Handle any errors
         console.error(error);
       });
   };
@@ -47,8 +65,8 @@ const Dashboard = () => {
       <h2>Dashboard</h2>
       {appraisals.map((appraisal) => (
         <div className="card" key={appraisal.id}>
-          <p>User ID: {appraisal.userId}</p>
-          <p>Overall Rating: {appraisal.overallRating}</p>
+          <p>User ID: {appraisal.employee_id}</p>
+          <p>Overall Rating: {appraisal.overall_rating}</p>
           <div className="btn-container">
             <button onClick={() => openPopup(appraisal)}>View</button>
             <button onClick={() => editAppraisal()}>Edit</button>
--- mp_appraisal_web_chatgpt/src/components/Login.js
+++ mp_appraisal_web_chatgpt/src/components/Login.js
@@ -1,56 +1,64 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const LoginPage = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const history = useHistory();
+  const [employee_id, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const history = useHistory();
 
-    const handleLogin = (e) => {
-        e.preventDefault();
-        // Perform login validation logic here
-        if (username === 'admin' && password === 'password') {
-            // Redirect to the appraisal page
-            history.push('/appraisal');
-        } else {
-            // Display login error message
-            alert('Invalid username or password');
-        }
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    const userDetails = { employee_id, password };
+    const url = "http://127.0.0.1:8000/" + "api/appraisalentry/employee_login";
+    const options = {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userDetails),
     };
-    
+    const response = await fetch(url, options);
+    const data = await response.json();
+    localStorage.setItem("userId", data[0].employee_id);
+    if (response.status === 200) {
+      localStorage.setItem("isAuthenticated", true);
+      history.push("/appraisal");
+    }
+  };
 
-    return (
-        <div className="login-container">
-            <h2>Login</h2>
-            <form onSubmit={handleLogin}>
-                <div>
-                    <label htmlFor="username">Username:</label>
-                    <input
-                        type="text"
-                        id="username"
-                        className="login-input"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                        required
-                    />
-                </div>
-                <div>
-                    <label htmlFor="password">Password:</label>
-                    <input
-                        type="password"
-                        id="password"
-                        className="login-input"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                    />
-                </div>
-                <button type="submit" className="login-button" >
-                    Login
-                </button>
-            </form>
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            className="login-input"
+            value={employee_id}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
         </div>
-    );
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            className="login-input"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit" className="login-button">
+          Login
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default LoginPage;
