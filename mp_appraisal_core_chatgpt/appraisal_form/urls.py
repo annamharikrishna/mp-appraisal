dc94eb86ee5bcbd02593dfd5ec1fd38cd5c992b8
Dasavanth-Alapati <111566135+Dasavanth-Alapati@users.noreply.github.com>
2023-05-18 12:57:34 +0530

    Update urls.py
    register url

--- mp_appraisal_core_chatgpt/appraisal_form/urls.py
+++ mp_appraisal_core_chatgpt/appraisal_form/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import AppraisalFormCreateView
+from .views import AppraisalFormCreateView, EmployeeRegistrationView
 
 urlpatterns = [
     path('create-appraisal-form/', AppraisalFormCreateView.as_view(), name='create_appraisal_form'),
+    path('register/', EmployeeRegistrationView.as_view(), name='register_employee'),
 ]
